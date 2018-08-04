3a4d46b5210401a7023aec67d1e67bc8d0ba2555
JPBlain <jpb-apps@orange.fr>
2018-08-04 08:37:56 +0200

    version 0.2
    mise en place de la navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostListItemComponent } from './post-list-item/post-list-item.component';
+import { NewPostComponent } from './new-post/new-post.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes} from '@angular/router';
+
+import { PostService } from './services/post.service'
+
+
+
+const appRoutes: Routes = [
+  { path: 'posts', component: PostListComponent },
+  { path: 'new', component: NewPostComponent },
+  { path: 'posts/view/:id', component: PostListItemComponent }
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostListComponent,
+    PostListItemComponent,
+    NewPostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [PostService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/models/post.model.ts
@@ -0,0 +1,9 @@
+class Post {
+  loveIts : number;
+  created_at : Date;
+
+  constructor(public title : string, public content: string) {
+    this.loveIts = 0;
+    this.created_at = new Date();
+  };
+}
